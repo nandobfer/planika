9ba2810dbca0e8aced20c781a3b1838026ee5014
nandobfer <nandobfer@yahoo.com.br>
2025-10-30 11:22:26 -0300

    loading

--- src/pages/Account/Invitations/TripFromParticipant.tsx
+++ src/pages/Account/Invitations/TripFromParticipant.tsx
@@ -2,13 +2,19 @@ import React from "react"
 import type { TripParticipant } from "../../../types/server/class/Trip/TripParticipant"
 import { useTrip } from "../../../hooks/useTrip"
 import { TripContainer } from "../../Trips/TripContainer/TripContainer"
+import { LinearProgress } from "@mui/material"
 
 interface TripFromParticipantProps {
     participant: TripParticipant
 }
 
 export const TripFromParticipant: React.FC<TripFromParticipantProps> = (props) => {
-    const { trip, acceptInvitation } = useTrip(props.participant.tripId)
+    const { trip, acceptInvitation, loading } = useTrip(props.participant.tripId)
 
-    return <TripContainer trip={trip} onAcceptInvite={acceptInvitation} />
+    return (
+        <>
+            {loading && <LinearProgress sx={{ position: "absolute", top: 0, left: 0, right: 0 }} />}
+            <TripContainer trip={trip} onAcceptInvite={acceptInvitation} />
+        </>
+    )
 }
