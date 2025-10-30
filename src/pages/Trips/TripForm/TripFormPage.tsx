3e2a7be77584c5af3ca6deb741f1b9e64047ca7f
nandobfer <nandobfer@yahoo.com.br>
2025-10-30 11:20:41 -0300

    loading

--- src/pages/Trips/TripForm/TripFormPage.tsx
+++ src/pages/Trips/TripForm/TripFormPage.tsx
@@ -1,33 +1,52 @@
-import React from "react"
-import { Box, Paper, Typography } from "@mui/material"
-import { useMuiTheme } from "../../../hooks/useMuiTheme"
+import React, { useEffect, useState } from "react"
+import { Box, LinearProgress } from "@mui/material"
 import { TripFormStepper } from "./TripFormStepper"
 import { useTripForm } from "../../../hooks/useTripForm"
 import { BasicInfoForm } from "./BasicInfoForm"
 import { ParticipantsForm } from "./ParticipantsForm"
 import { FinishedForm } from "./FinishedForm"
+import { EventBus } from "../../../tools/EventBus"
+import { useMuiTheme } from "../../../hooks/useMuiTheme"
 
 interface TripFormPageProps {}
 
 export const TripFormPage: React.FC<TripFormPageProps> = (props) => {
-    // const { gradientStyle, invertedGradientStyle } = useMuiTheme()
+    const { disabledStyle } = useMuiTheme()
     const tripForm = useTripForm()
 
+    const [loading, setLoading] = useState(false)
+
+    const handleTripsLoading = (value: boolean) => {
+        setLoading(value)
+    }
+
+    useEffect(() => {
+        EventBus.on("trip-loading", handleTripsLoading)
+
+        return () => {
+            EventBus.off("trip-loading", handleTripsLoading)
+        }
+    }, [])
+
     return (
-        <Box sx={{ flexDirection: "column", gap: 5, flex: 1 }}>
-            <TripFormStepper tripForm={tripForm} />
-            <Box
-                sx={{
-                    flexDirection: "column",
-                    // padding: 5,
-                    // flex: 1,
-                    // ...gradientStyle,
-                }}
-            >
-                {tripForm.step === 0 && <BasicInfoForm tripForm={tripForm} />}
-                {tripForm.step === 1 && <ParticipantsForm tripForm={tripForm} />}
-                {tripForm.step === 2 && <FinishedForm tripForm={tripForm} />}
+        <>
+            {loading && <LinearProgress sx={{ position: "absolute", top: 0, left: 0, right: 0 }} />}
+            {loading && <LinearProgress sx={{ position: "absolute", bottom: 0, left: 0, right: 0 }} />}
+            <Box sx={{ flexDirection: "column", gap: 5, flex: 1, ...(loading ? disabledStyle : {}) }}>
+                <TripFormStepper tripForm={tripForm} />
+                <Box
+                    sx={{
+                        flexDirection: "column",
+                        // padding: 5,
+                        // flex: 1,
+                        // ...gradientStyle,
+                    }}
+                >
+                    {tripForm.step === 0 && <BasicInfoForm tripForm={tripForm} />}
+                    {tripForm.step === 1 && <ParticipantsForm tripForm={tripForm} />}
+                    {tripForm.step === 2 && <FinishedForm tripForm={tripForm} />}
+                </Box>
             </Box>
-        </Box>
+        </>
     )
 }
